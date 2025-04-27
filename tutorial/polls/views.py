5688443a04cd23291ea434d76920bd2dac177006
Charlie <charlie2265@gmail.com>
2025-04-27 07:55:48 -0700

    Generic veiws

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,6 +1,65 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import F
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Choice, Question
 
 # Create your views here.
-def index(requests):
-    return HttpResponse("hello, world. You're at the polls index.")
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(request, 
+                      "polls/details.html",
+                      {
+                          "question": question,
+                          "error_message": "you didnt select a choice.",
+                      },
+                      )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponse after successfully dealing 
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+
+
+
+
+# # There is a shortcut for this view to raise a 404 in the django docs tutorial 3
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+#     # return HttpResponse("You're looking at question %s." % question_id)
+
+
+    
+
+# def results(request, question_id):
+#     # response = "You're looking at question %s."
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
